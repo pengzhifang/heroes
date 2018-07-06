444e56a8bff9eb19154d8c2f1c27da5e6aa33b4a
peng zhifang <1318255539@qq.com>
2018-07-06 10:32:08 +0800

    项目初始化结构

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="root">
+    hello world111
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
