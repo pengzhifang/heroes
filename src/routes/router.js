938a45156d380ebf5c4a192cecb60048f7074ea6
peng zhifang <1318255539@qq.com>
2018-07-07 13:13:11 +0800

    添加英雄路由

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import HeroList from '../views/heros/List.vue';
 import WeaponList from '../views/weapons/List.vue';
 import EquipList from '../views/equips/List.vue';
+import HeroAdd from '../views/heros/HeroAdd.vue';
 
 Vue.use(VueRouter);
 
@@ -13,7 +14,8 @@ const router = new VueRouter({
       {name: 'home', path: '/', redirect: '/heros'},
       {name: 'heros', path: '/heros', component: HeroList},
       {name: 'weapons', path: '/weapons', component: WeaponList},
-      {name: 'equips', path: '/equips', component: EquipList}
+      {name: 'equips', path: '/equips', component: EquipList},
+      {name: 'heroadd', path: '/heros/add', component: HeroAdd}
    ]
 });
 
---
+++ src/views/heros/HeroAdd.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+      <h2 class="sub-header">Add Hero</h2>
+      <form>
+         <div class="form-group">
+         <label for="name">英雄名称</label>
+         <input type="text" class="form-control" id="name" placeholder="name">
+         </div>
+         <div class="form-group">
+         <label for="sex">英雄性别</label>
+         <input type="text" class="form-control" id="sex" placeholder="sex">
+         </div>
+         
+         <button type="submit" class="btn btn-success">Submit</button>
+      </form>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
