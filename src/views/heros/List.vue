fea3b322ab64927f95e98e8e48b5fd4a6cf9553c
peng zhifang <1318255539@qq.com>
2018-07-07 13:00:40 +0800

    删除英雄

--- src/views/heros/List.vue
+++ src/views/heros/List.vue
@@ -14,13 +14,14 @@
             </thead>
             <tbody>
                <tr :key="item.id" v-for="(item, index) in list">
-                  <td>{{ index }}</td>
+                  <td>{{ index + 1 }}</td>
                   <td>{{ item.name }}</td>
                   <td>{{ item.gender }}</td>
                   <td>
                      <a href="edit.html">edit</a>
                      &nbsp;&nbsp;
-                     <a href="javascript:window.confirm('Are you sure?')">delete</a>
+                     <!-- <a href="javascript:window.confirm('Are you sure?')">delete</a> -->
+                     <a href="#" @click.prevent="handleDelete(item.id)">delete</a>
                   </td>
                </tr>
                
@@ -53,6 +54,19 @@ export default {
             .catch((err) => {
                console.log(err);
             })
+      },
+      handleDelete(id) {
+			if (!confirm('您确定删除吗')) {
+				return;
+			}
+         axios
+            .delete(`http://localhost:3000/heroes/${id}`)
+            .then((res) => {
+					this.loadData();
+				})
+				.catch((err) => {
+					console.log(err);
+				})
       }
    }
 }
